04249c0a346476e35347b3abe3b4a3d48a211408
Yossi Eynav <yossi.eynav@gmail.com>
2017-03-12 22:45:18 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import todoApp from './reducers'
-import App from './components/App'
+import Canvas from './components/Canvas'
 
-let store = createStore(
-  todoApp, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunkMiddleware)
-  );
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <Canvas />,
   document.getElementById('root')
 )
