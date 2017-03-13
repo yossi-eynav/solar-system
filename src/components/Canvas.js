8a7eac1bb8ece7f3e244b52d97e99b1dd587072f
Yossi Eynav <yossi.eynav@gmail.com>
2017-03-13 11:59:38 +0200

    wip

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,43 +1,52 @@
 import React from 'react'
 import '../components/Canvas.scss'
 
-var x = 1;
-let y = 0.001;
-
-
 class Canvas extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.drawCanvas = this.drawCanvas.bind(this);
+    } 
+    
+    componentDidMount(){
+       const canvas = this.refs.canvas;
+       this.ctx = canvas.getContext('2d')
+       this.meteorX = 1;
+       this.meteorY = 0.001;
+       this.drawCanvas();
+    }
 
-    func() {
-            const canvas = this.refs.canvas;
-        const ctx = canvas.getContext('2d')
-        //    ctx.globalCompositeOperation = 'destination-over';
- ctx.restore()
-        ctx.clearRect(0, 0, 800, 800); // clear canvas
+    drawCanvas() {
+        const ctx = this.ctx,
+            windowWidth = window.innerWidth,
+            time = new Date();
 
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+        let img;
+
+        ctx.restore()
+        ctx.clearRect(0, 0, windowWidth, windowWidth); // clear canvas
         ctx.save();
 
-var img = new Image();   // Create new img element
-img.src = 'galaxy.jpg'
-    ctx.drawImage(img, -100, 0, 1400, 800);
+        img = new Image();
+        img.src = 'galaxy.jpg'
+        ctx.drawImage(img, -100, 0, 1400, windowWidth);
 
+        ctx.translate(windowWidth / 2, windowWidth /2);
+        ctx.save();          
+        ctx.beginPath();
+        ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+        ctx.arc(0,0,200,0, Math.PI *2)
+        ctx.stroke()
 
-
-
-        ctx.translate(400, 400);
-                ctx.save();
-        var time = new Date();
-var img = new Image();   // Create new img element
-img.src = 'sun.png'
+        img = new Image();
+        img.src = 'sun.png'
         ctx.rotate( 2 * Math.PI / 60 * (time.getSeconds() +  time.getMilliseconds() / 1000) );
 
-    ctx.drawImage(img, -40, -40, 80, 80);
 
+        ctx.drawImage(img, -40, -40, 80, 80);
         ctx.rotate( 2 * Math.PI / 60 * (time.getSeconds() +  time.getMilliseconds() / 1000) );
-        ctx.fillStyle = 'red';
         ctx.translate(0, 190);
-        var img = new Image();   // Create new img element
+        img = new Image();
         img.src = 'earth.png'
         ctx.drawImage(img, -30, -30, 60, 60);
         ctx.save();
@@ -47,51 +56,40 @@ img.src = 'sun.png'
         ctx.fillStyle = 'red';
         ctx.translate(-30, -60);
         
-        var img = new Image();   // Create new img element
+        img = new Image();
         img.src = 'moon.png'
         ctx.drawImage(img, -15, -15, 30, 30);
-        // ctx.fillRect(0, 0, 20, 20); // Shadow
+
         ctx.restore();
         ctx.restore();
         ctx.save();
 
         ctx.rotate( 2 * Math.PI / 60 * ((time.getSeconds() +  time.getMilliseconds() / 1000)) * 1 );
-        ctx.fillStyle = 'red';
         ctx.translate(0, 300);
-    
-
 
         ctx.restore();
-        ctx.beginPath();
-        ctx.arc(0,0,200,0, Math.PI *2)
-        ctx.stroke()
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
+      
 
-        var img = new Image();   // Create new img element
+        img = new Image();
         img.src = 'meteor.png'
         ctx.restore()
        
-        if (x < 2000) { 
-            console.log(x)
-             ctx.drawImage(img,x - 50, y + 10, 40, 40);
-             ctx.drawImage(img,x * 1.4 - 50, y+ 30, 20, 20);
-             ctx.drawImage(img,x * 0.5 - 70, y +20, 60, 60);
-             ctx.drawImage(img,x * 1.15 - 50, y + 50, 25, 25);
+        if (this.meteorX < 2000) { 
+             ctx.drawImage(img,this.meteorX - 50, this.meteorY + 10, 40, 40);
+             ctx.drawImage(img,this.meteorX * 1.4 - 50, this.meteorY + 30, 20, 20);
+             ctx.drawImage(img,this.meteorX * 0.5 - 70, this.meteorY +20, 60, 60);
+             ctx.drawImage(img,this.meteorX * 1.15 - 50, this.meteorY + 50, 25, 25);
 
-            x *= 1.016
-            y += 0.2;
+            this.meteorX *= 1.016
+            this.meteorY += 0.2;
         }
-    
 
-        window.requestAnimationFrame(this.func.bind(this))
-    }
-    
-    componentDidMount() {
-        this.func();
+        window.requestAnimationFrame(this.drawCanvas)
     }
 
     render() {
-        return (<canvas width="800" height="800" ref="canvas"> </canvas>)
+        const windowWidth = window.innerWidth
+        return (<canvas width={windowWidth} height={windowWidth} ref="canvas"> </canvas>)
     }
 }
 
